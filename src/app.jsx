1bdbbbc1ea916ad61a491c667b675b225fa29b4f
ayoubdev <ayoub.development@gmail.com>
2016-02-23 22:32:47 +0100

    => Added react-router dependancy => Defined react-router routes template with navigation links inside app.jsx => Added welcome component to show params interaction

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { browserHistory, Router, Route, Link } from 'react-router';
 import ExampleComponent from './components/example/example.jsx';
 import HeaderComponent from './components/header/header.jsx';
+import WelcomeComponent from './components/welcome/welcome.jsx';
 
 let styles = {
 	color: 'white',
@@ -10,15 +12,52 @@ let styles = {
 
 class App extends React.Component {
 	render() {
+		console.log(this.props.children);
 		return (
 			<div>
-				<HeaderComponent/>
-				
-				<p style={styles}> Hello React!</p>
-				<ExampleComponent/>
+				<h1 style={styles}> React template with Webpack, react-router and react-hot-loader </h1>
+				<ul>
+					<li><Link to="/example">Click to see Example component</Link></li>
+					<li><Link to="/header">Click To see Header component</Link></li>
+					<li><Link to="/welcome/Ayoub/23">Click To see Welcome component</Link></li>
+				</ul>
+				{this.props.children}
 			</div>
 		);
 	}
 }
 
-render(<App/>, document.getElementById('app'));
+/*let routes = (
+	<Router history={browserHistory}>
+		<Route path="/" component={App}/>
+	</Router>
+);*/
+render((
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			{/*Les composants spécifiés dans les routes enfants seront ajoutés dans
+			props.children du composant de la route parent (soit ici App.props.children)
+			C'est la raison pour laquelle dans le render de App, on a ajouté
+			{this.props.children} pour afficher les enfants*/}
+			<Route path="example" component={ExampleComponent} />
+			<Route path="header" component={HeaderComponent} />
+			<Route path="welcome/">
+				{/*inutile de spécifier le component dans <Route path=":age"/>
+				puisqu'on reste dans le contexte (composant) de la route parent*/}
+				<Route path=":name/" component={WelcomeComponent}>
+					<Route path=":age"/>
+				</Route>
+				{/*//ou:
+				 <Route path=":name" component={WelcomeComponent} />
+				 <Route path=":name/:age" component={WelcomeComponent} />
+				*/}
+			</Route>
+		</Route>
+
+		{/*No match route (route par défaut si non trouvé):*/}
+		<Route path="*" component={ExampleComponent}/>
+	</Router>
+), document.getElementById('app'));
+
+//render par défaut sans react-router:
+//render(<App/>, document.getElementById('app'));
---
+++ src/components/welcome/welcome.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class WelcomeComponent extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		let params = this.props.params;
+		return (
+			<p> Welcome {params.name} to React world ! [You are {params.age} years old] </p>
+		);
+	}
+}
+
+export default WelcomeComponent;
