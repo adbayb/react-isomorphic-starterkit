95913e74935fa3ed9cc9f2712035a3162828b0a8
ayoubdev <ayoub.development@gmail.com>
2016-02-29 23:00:23 +0100

    => Miscs

--- src/server.jsx
+++ src/server.jsx
@@ -32,13 +32,13 @@ export default function(req, res) {
 	*/
 	match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
 		if(error) {
-			res.status(500).send(error.message)
+			res.status(500).send(error.message);
 		} else if(redirectLocation) {
-			res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+			res.redirect(302, redirectLocation.pathname + redirectLocation.search);
 		} else if(renderProps) {
-			res.status(200).send(renderHTML(renderToString(<RouterContext {...renderProps} />)))
+			res.status(200).send(renderHTML(renderToString(<RouterContext {...renderProps} />)));
 		} else {
-			res.status(404).send('Not found')
+			res.status(404).send('Not found');
 		}
 	});
 };
