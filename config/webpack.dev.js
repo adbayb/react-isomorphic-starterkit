386180f7188d75ab4a345e5fc2ce497b2ce75608
ayoubdev <ayoub.development@gmail.com>
2016-06-01 23:27:58 +0200

    => config/webpack.dev.js: Fixed HMR not working => config/webpack.dev.js: Commented server watch files => package.json: Updated postinstall command => package.json: Separated commands from test by creating test2 command (draft)

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,6 +7,7 @@ var clientWebpack = require("./webpack.shared.js").client;
 //webpackConfig.client.plugins.push(new webpack.HotModuleReplacementPlugin());
 //webpackConfig.client.module.loaders[0].loaders.push("react-hot");
 
+/*
 var loaders = [{
 	test: /\.js[x]?$/,
 	include: clientWebpack.entry,
@@ -51,30 +52,89 @@ var devWebpack = Object.assign({},
 			//new ExtractTextPlugin("client.bundle.css")
 		]
 	}
-);
+);*/
+var path = require("path");
+var APP_DIR = path.resolve(__dirname, "..", "src");
+
+//La configuration Webpack côté serveur ne gérera que l'arbre de dépendance javascript
+//Les arbres de dépendances des ressources (css, html, images...) seront gérés par la configuration client
+//pour éviter une double gestion des ressources côté serveur et client.
+//Cependant, Webpack se base sur l'arbre des requires pour injecter les liens dans le code javascript
+//vers les bonnes ressources required. Il faut donc quand même gérer les ressources côté serveur.
+//En output, on aura deux dossiers (client et serveur), le fichier js côté serveur se chargera
+//via Express de loader les assets depuis le dossier client.
+//Le template html serveur se charge de loader les ressources clients bundlées.
+var loaders = [{
+	test: /\.js[x]?$/,
+	include: APP_DIR,
+	loaders: [
+		"react-hot-loader",
+		"babel-loader?" + JSON.stringify({
+			presets: ["es2015", "react"]
+		})
+	]
+}, {
+	test: /\.css$/,
+	loader: "style-loader!css-loader?modules&localIdentName=[path]-[name]_[local]-[hash:base64:5]"
+}, {
+	test: /\.scss$/,
+	loader: "style-loader!css-loader?modules&localIdentName=[path]-[name]_[local]-[hash:base64:5]!sass-loader"
+}, {
+	test: /\.(jp[e]?g|png|gif|svg)$/i,
+	loader: "file-loader?name=img/[name].[ext]"
+}, {
+	test: /\.html$/,
+	loader: "file-loader?name=[name].[ext]"
+}, {
+	test: /\.ico$/,
+	loader: "file-loader?name=[name].[ext]"
+}];
+
+console.log(__dirname);
+
+var devWebpack = {
+	name: "client-webpack",
+	//Par défaut, Webpack set l'environnement cible à "web":
+	target: "web",
+	/*entry: {
+		APP_DIR + "/client"
+	},*/
+	entry: [
+		"webpack/hot/only-dev-server",
+		"webpack-dev-server/client?http://localhost:8081", //WebpackDevServer host et port
+		//"only" permet d"empêcher le rechargement lors d"erreurs de syntaxes
+		//fichier d"entrée principale de notre code source (client et non client.jsx comme le dossier
+		//contient index.jsx donc automatiquement loadé lorsque l"on spécifie le dossier):
+		APP_DIR + "/client"
+	],
+	output: {
+		filename: "client.bundle.js",
+		path: __dirname,
+		publicPath: "http://localhost:8081/"
+	},
+	module: {
+		loaders: loaders
+	},
+	resolve: {
+		extensions: ["", ".js", ".jsx"]
+	},
+	plugins: [
+		//new ExtractTextPlugin("client.bundle.css")
+		new webpack.HotModuleReplacementPlugin()
+	]
+};
+
+
+
+
+
+
 
 var WebpackDevServer = require("webpack-dev-server");
 
-var statOptions = {
-	colors: true,
-	hash: false,
-	timings: false,
-	chunks: false,
-	chunkModules: false,
-	modules: false,
-	children: true,
-	version: true,
-	cached: false,
-	cachedAssets: false,
-	reasons: false,
-	source: false,
-	errorDetails: false
-};
-
 new WebpackDevServer(webpack(devWebpack), {
 	hot: true,
-	historyApiFallback: true,
-	stats: statOptions
+	historyApiFallback: true
 }).listen(8081, "localhost", function(err) {
 	if(err) {
 		console.log(err);
@@ -82,10 +142,10 @@ new WebpackDevServer(webpack(devWebpack), {
 	console.log("Webpack Server launched with at localhost:8081 (Hot Module Replacement [HMR] enabled)");
 });
 
-var serverWebpack = require("./webpack.shared.js").server;
+/*var serverWebpack = require("./webpack.shared.js").server;
 webpack(serverWebpack).watch({}, function(err, stats) {
 	if (err) return console.error(err.message);
 	console.log(stats.toString(statOptions));
-});
+});*/
 
 //module.exports = devWebpack;
